6828771aa2c304a8860568c1d108157ede80987f
Keerthana2501 <34852049+Keerthana2501@users.noreply.github.com>
2018-04-03 12:00:30 +0530

    Update findvowels.c

--- findvowels.c
+++ findvowels.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
- 
+ #include<string.h>
 int main()
 {
-    char ch;
- 
+    char ch[20];
+ int i;
     printf("Please Enter an alphabet: \n");
-    scanf(" %c", &ch);
-    
-    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-		ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')  {
-		printf("\n %c is a VOWEL.", ch);
+    scanf(" %s", &ch);
+    for(i=0;i<strlen(ch);i++){
+    if(ch[i] == 'a' ||ch[i] == 'e' ||ch[i] == 'i' || ch[i] == 'o' ||ch[i]== 'u' ||
+		ch[i]== 'A' || ch[i]== 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U')  {
+		printf("\n %c is a VOWEL.", ch[i]);
 	}
     else {
-    	printf("\n %c is a CONSONANT.", ch);
+    	printf("\n %c is a CONSONANT.", ch[i]);
 	}
     return 0;
+    }
 }
